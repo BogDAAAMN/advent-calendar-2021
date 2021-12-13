6500c36d7504b4a8d92b871901f891042b5bf2af
Bogdan Covrig <bogcov@gmail.com>
2021-12-13 18:54:02 +0100

    graphql: Add stepzen API call in getData

--- advent-of-graphql/unsplash-snowflake-ui/pages/api/snowflake.js
+++ advent-of-graphql/unsplash-snowflake-ui/pages/api/snowflake.js
@@ -1,9 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
+export default async function getData(req, res) {
+  const stepzenResponse = await fetch(process.env.STEPZEN_API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `apikey ${process.env.STEPZEN_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query GetRandomPhoto {unsplash_Random_Photo {
+          urls {
+            full
+          }
+        }
+      }`,
+    }),
+  });
 
-export default async function getData(req, res) { 
-  
+  let data = await stepzenResponse.json();
+  return res.json({ data: data });
 }
